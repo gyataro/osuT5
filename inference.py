3137ff1a41a27a122b5fb8289a4c68193f71e670
Xiwen Teoh <teoh6g@gmail.com>
2022-12-14 15:16:30 +0800

    feat: generate .osu beatmap file

--- inference.py
+++ inference.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import uuid
+import dataclasses
+from string import Template
+
 import torch
 import numpy as np
 import numpy.typing as npt
@@ -12,12 +16,37 @@ from data.tokenizer import Tokenizer
 from data.event import Event, EventType
 from config.config import Config
 
+OSU_FILE_EXTENSION = ".osu"
 MILISECONDS_PER_STEP = 10
 MILISECONDS_PER_SECOND = 1000
 
 
+@dataclasses.dataclass
+class BeatmapMetadata:
+    audio_filename: str = "audio.mp3"
+    title: str = "osu_beatmap"
+    artist: str = "osu_transformer"
+    title_unicode: str = "osu_beatmap"
+    artist_unicode: str = "osu_transformer"
+
+
+@dataclasses.dataclass
+class BeatmapDifficulty:
+    hp_drain_rate: float = 5
+    circle_size: float = 5
+    overall_difficulty: float = 5
+    approach_rate: float = 5
+    slider_multiplier: float = 1.4
+
+
+@dataclasses.dataclass
+class BeatmapTiming:
+    offset: int = 0
+    beat_length: float = 120
+
+
 class Preprocessor(object):
-    def __init__(self, config):
+    def __init__(self, config: Config):
         """Preprocess audio data into sequences."""
         self.frame_seq_len = config.dataset.src_seq_len - 1
         self.frame_size = config.spectrogram.hop_length
@@ -58,7 +87,7 @@ class Preprocessor(object):
 
 
 class Pipeline(object):
-    def __init__(self, config):
+    def __init__(self, config: Config):
         """Model inference stage that processes sequences."""
         self.tokenizer = Tokenizer()
         self.tgt_seq_len = config.dataset.tgt_seq_len
@@ -70,7 +99,9 @@ class Pipeline(object):
             self.samples_per_sequence * MILISECONDS_PER_SECOND / self.sample_rate
         )
 
-    def generate(self, model: OsuTransformer, sequences: torch.Tensor):
+    def generate(
+        self, model: OsuTransformer, sequences: torch.Tensor
+    ) -> tuple[list[list[Event]], list[int]]:
         """Generate a list of Event object lists and their timestamps given source sequences.
 
         Args:
@@ -100,15 +131,15 @@ class Pipeline(object):
                 tgt = torch.cat([tgt, token.unsqueeze(-1)], dim=-1)
 
             result = self._decode(tgt[0], index)
-            events.append(result[0])
-            event_times.append(result[1])
+            events += result[0]
+            event_times += result[1]
 
         return events, event_times
 
     def _decode(
         self, tokens: torch.Tensor, index: int
     ) -> tuple[list[list[Event]], list[int]]:
-        """Convers a list of tokens into Event object lists and their timestamps.
+        """Converts a list of tokens into Event object lists and their timestamps.
 
         Args:
             tokens: List of tokens.
@@ -125,7 +156,7 @@ class Pipeline(object):
             elif token == self.tokenizer.eos_id:
                 break
 
-            event = self.tokenizer.decode(token)
+            event = self.tokenizer.decode(token.item())
 
             if event.type == EventType.TIME_SHIFT:
                 timestamp = index * self.miliseconds_per_sequence + event.value
@@ -169,8 +200,84 @@ class Pipeline(object):
 
 
 class Postprocessor(object):
-    def __init__(self):
-        """Generate a beatmap from a list of Event objects."""
+    def __init__(self, config: Config):
+        """Postprocessing stage that converts a list of Event objects to a beatmap file."""
+        self.curve_types = {
+            EventType.SLIDER_BEZIER: "B",
+            EventType.SLIDER_CATMULI: "C",
+            EventType.SLIDER_LINEAR: "L",
+            EventType.SLIDER_PERFECT_CIRCLE: "P",
+        }
+
+    def generate(
+        self,
+        events: list[list[Event]],
+        event_times: list[int],
+        metadata: BeatmapMetadata,
+        difficulty: BeatmapDifficulty,
+        timing: BeatmapTiming,
+    ):
+        """Generate a beatmap file.
+
+        Args:
+            events: List of Event object lists.
+            event_times: Corresponding event times of Event object lists in miliseconds.
+            metadata: Beatmap metadata
+            difficulty: Beatmap difficulty configuration
+            timing: Beatmap timing configuration
+
+        Returns:
+            None. An .osu file with a random UUID filename will be generated.
+        """
+
+        hit_object_strings = []
+
+        for hit_object, timestamp in zip(events, event_times):
+            print(hit_object)
+            x = hit_object[0].value
+            y = hit_object[1].value
+            hit_type = hit_object[2].type
+
+            if hit_type == EventType.CIRCLE:
+                hit_object_strings.append(f"{x},{y},{timestamp},1,0")
+
+            elif hit_type in self.curve_types:
+                curve_type = self.curve_types[hit_type]
+                slides = 1
+                control_points = ""
+
+                if hit_object[-1].type == EventType.SLIDES:
+                    slides = hit_object[-1].value
+
+                for i in range(3, len(hit_object) - 1, 2):
+                    if (
+                        hit_object[i].type == EventType.CONTROL_POINT
+                        and hit_object[i + 1].type == EventType.CONTROL_POINT
+                    ):
+                        control_points += (
+                            f"|{hit_object[i].value}:{hit_object[i+1].value}"
+                        )
+
+                hit_object_strings.append(
+                    f"{x},{y},{timestamp},2,0,{curve_type}{control_points},{slides}"
+                )
+
+        with open("template.osu", "r") as tf:
+            template = Template(tf.read())
+            metadata = dataclasses.asdict(metadata)
+            difficulty = dataclasses.asdict(difficulty)
+            timing = dataclasses.asdict(timing)
+            hit_objects = {"hit_objects": "\n".join(hit_object_strings)}
+
+            result = template.safe_substitute(
+                {**metadata, **difficulty, **timing, **hit_objects}
+            )
+
+            with open(f"{uuid.uuid4().hex}{OSU_FILE_EXTENSION}", "w") as f:
+                f.write(result)
+                f.close()
+
+            tf.close()
 
 
 if __name__ == "__main__":
@@ -179,7 +286,7 @@ if __name__ == "__main__":
     config = Config()
 
     checkpoint = torch.load(
-        "./lightning_logs/checkpoint.ckpt",
+        "./checkpoint.ckpt",
         map_location=torch.device("cpu"),
     )
 
@@ -187,12 +294,17 @@ if __name__ == "__main__":
     model.load_state_dict(checkpoint["state_dict"])
     model.eval()
 
-    preprocessor = Preprocessor(config)
     pipeline = Pipeline(config)
+    preprocessor = Preprocessor(config)
+    postprocessor = Postprocessor(config)
 
     audio = preprocessor.load("./audio.mp3")
     sequences = preprocessor.segment(audio)
     events, event_times = pipeline.generate(model, sequences)
 
-    print(events)
-    print(event_times)
+    beatmap_metadata = BeatmapMetadata()
+    beatmap_difficulty = BeatmapDifficulty()
+    beatmap_timing = BeatmapTiming()
+    postprocessor.generate(
+        events, event_times, beatmap_metadata, beatmap_difficulty, beatmap_timing
+    )
