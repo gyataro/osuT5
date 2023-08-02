7c0837425989918f812d2cae54bbf1b7301e8846
Xiwen Teoh <teoh6g@gmail.com>
2023-08-02 16:41:47 +0800

    feat: combos and spinners

--- osuT5/tokenizer/event.py
+++ osuT5/tokenizer/event.py
@@ -7,6 +7,7 @@ from enum import Enum
 class EventType(Enum):
     TIME_SHIFT = "t"
     POINT = "p"
+    NEW_COMBO = "new_combo"
     CIRCLE = "circle"
     SLIDER_BEZIER = "slide_b"
     SLIDER_CATMULI = "slide_c"
@@ -14,6 +15,8 @@ class EventType(Enum):
     SLIDER_PERFECT_CIRCLE = "slide_p"
     CONTROL_POINT = "cp"
     SLIDES = "slides"
+    SPINNER = "spinner"
+    SPINNER_LENGTH = "spinner_length"
 
 
 @dataclasses.dataclass
@@ -40,10 +43,13 @@ def get_event_ranges() -> list[EventRange]:
         EventRange(EventType.TIME_SHIFT, 0, 512),
         EventRange(EventType.POINT, 0, 512),
         EventRange(EventType.CIRCLE, 0, 0),
+        EventRange(EventType.NEW_COMBO, 0, 1),
         EventRange(EventType.SLIDER_BEZIER, 0, 0),
         EventRange(EventType.SLIDER_CATMULI, 0, 0),
         EventRange(EventType.SLIDER_LINEAR, 0, 0),
         EventRange(EventType.SLIDER_PERFECT_CIRCLE, 0, 0),
-        EventRange(EventType.CONTROL_POINT, -128, 640),
+        EventRange(EventType.CONTROL_POINT, -256, 768),
         EventRange(EventType.SLIDES, 0, 100),
+        EventRange(EventType.SPINNER, 0, 0),
+        EventRange(EventType.SPINNER_LENGTH, 0, 512),
     ]
